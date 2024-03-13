daf5a695f19bdd77b9eacf3b2726cae4623bb9d2
Mans Léo <leo.mans52@gmail.com>
2024-03-13 11:51:43 +0100

    Ajout detection ligne de depart/arrive + avancer avec correction de trajectoire

--- etape1_simu.py
+++ etape1_simu.py
@@ -11,7 +11,7 @@ class RobotException(Exception):
     """
     message = ""
     line_number=""
-    def __init__(self, message, line_number):
+    def __init__(self, message, line_number=""):
         super().__init__()
         self.message = message
         self.line_number = line_number
@@ -25,7 +25,9 @@ class Robot :
     capteurCouleur = None
     bouton1 = None
     bouton2 = None
-    def __init__(self, L_motor1, L_motor2,M_motor,colorSensor,ultrasonSensor,but_1,but_2):
+    gyroscope = None
+    moteurs = None 
+    def __init__(self, L_motor1, L_motor2,M_motor,colorSensor,ultrasonSensor,giroSensor,but_1,but_2):
         try :
             if(L_motor1 != None) :
                 self.moteurGauche = LargeMotor(L_motor1)
@@ -37,45 +39,157 @@ class Robot :
                 self.capteurCouleur = ColorSensor(colorSensor)
             if(ultrasonSensor != None) :
                 self.capteurUltrason= UltrasonicSensor(ultrasonSensor)
+            if(giroSensor != None) :
+                self.gyroscope= GyroSensor(giroSensor)
             if(but_1 != None) :
                 self.bouton1 = TouchSensor(but_1)
             if(but_2 != None) :
                 self.bouton2 = TouchSensor(but_2)
+            if (L_motor1 != None and L_motor2 != None) :
+                self.moteurs = MoveSteering(L_motor1,L_motor2)
         except Exception as e : 
             line_number = str(e).split(" ")[-1]  # Extraire le numéro de ligne de la chaîne de traceback
             raise RobotException("Erreur lors de la création de l'objet robot \nErreur: " + str(e),line_number )
     
     def preparationCouleur(self) :
-        tentatives = 0
-        not_calibrated = True
-        while (not_calibrated or tentatives < 10):
-            try:
-                self.capteurCouleur.calibrate_white()
-                # not good if one of the max is zero
-                not_calibrated = ( (self.capteurCouleur.red_max == 0) 
-                                   or (self.capteurCouleur.green_max == 0)
-                                   or (self.capteurCouleur.blue_max == 0) )
-            except:
-                not_calibrated = True
-        if(not_calibrated) :
-            line_number = str(e).split(" ")[-1]  # Extraire le numéro de ligne de la chaîne de traceback
-            raise RobotException("Erreur lors du calibrage du capteur couleur",line_number)
-        
-                
-                
-    def avancer(self,distance):
-        self.moteurGauche.run_to_rel_pos(position_sp=distance, speed_sp=500, stop_action="hold")
-        self.moteurDroite.run_to_rel_pos(position_sp=distance, speed_sp=500, stop_action="hold")
-        self.moteurGauche.wait_until_not_moving()
-        self.moteurDroite.wait_until_not_moving()                
+        if not self.verificationCapteurCouleur() :
+            raise RobotException("Le robot n'a pas de capteur couleur définis")
 
+  
+        # Calibrage blanc
+        self.capteurCouleur.calibrate_white
+
+    def  getCouleur(self) :
+        r,g,b = self.capteurCouleur.rgb
+        return (r,g,b)
+    def isCouleurNoir(self,limite) :
+        couleur = self.getCouleur()
+        if (couleur[0] > limite and couleur[1] > limite and couleur[2] > limite) :
+            return False
+        else :
+            return True
+
+    def preparationRobot(self,) :
+        if (self.verificationCapteurCouleur()) :
+            self.preparationCouleur()
+        if(self.verificationGyroscope) :
+            self.preparationGyroscope
+    def preparationGyroscope(self) :
+        self.gyroscope.calibrate()
+    def verificationCapteurCouleur(self) :
+        return True if self.capteurCouleur is not None else False
+    def verificationCapteurUltrason(self) :
+        return True if self.capteurUltrason is not None else False
+    def verificationMoteurDroite(self) :
+        return True if self.moteurDroite is not None else False
+    def verificationMoteurGauche(self) :
+        return True if self.moteurGauche is not None else False
+    def verificationBouton1(self) :
+        return True if self.bouton1 is not None else False
+    def verificationBouton2(self) :
+        return True if self.bouton2 is not None else False            
+    def verificationGyroscope(self) :
+        return True if self.gyroscope is not None else False    
+    def avancerLigneDepart(self) :
+        SEUIL_NOIR = 100
+        while( not self.isCouleurNoir(SEUIL_NOIR) ) :
+            self.avancer()
+            print(self.getCouleur())
+        print("Ligne de départ atteinte !")
+        while(self.isCouleurNoir(SEUIL_NOIR)) :
+            self.avancer()
+            print(self.getCouleur())   
+        print("Ligne de départ traverse !")
+        self.stop()
+    def reculerLigneDepart(self) :
+        SEUIL_NOIR = 100
+        while( not self.isCouleurNoir(SEUIL_NOIR) ) :
+            self.reculer()
+            print(self.getCouleur())
+        print("Ligne de départ atteinte !")
+        while(self.isCouleurNoir(SEUIL_NOIR)) :
+            self.reculer()
+            print(self.getCouleur())   
+        print("Ligne de départ traverse !")
+        self.stop()
+    def avancerLigneArrivee(self) :
+        SEUIL_NOIR = 100
+        while( not self.isCouleurNoir(SEUIL_NOIR) ) :
+            # Calcul a faire (etape2)
+            self.avancer()
+            print(self.getCouleur())
+        print("Ligne d'arrivée atteinte !")
+        while(self.isCouleurNoir(SEUIL_NOIR)) :
+            self.avancer()
+            print(self.getCouleur())   
+        print("Ligne d'arrivée traverse !")
+        self.stop()
+    def get_moteurGauche(self):
+        return self.moteurGauche
+
+    def set_moteurGauche(self, value):
+        self.moteurGauche = value
+
+    def get_moteurDroite(self):
+        return self.moteurDroite
+
+    def set_moteurDroite(self, value):
+        self.moteurDroite = value
+
+    def get_moteurSecondaire(self):
+        return self.moteurSecondaire
+
+    def set_moteurSecondaire(self, value):
+        self.moteurSecondaire = value
+
+    def get_capteurCouleur(self):
+        return self.capteurCouleur
+
+    def set_capteurCouleur(self, value):
+        self.capteurCouleur = value
+
+    def get_bouton1(self):
+        return self.bouton1
+
+    def set_bouton1(self, value):
+        self.bouton1 = value
+
+    def get_bouton2(self):
+        return self.bouton2
+
+    def set_bouton2(self, value):
+        self.bouton2 = value
+
+    def get_gyroscope(self):
+        return self.gyroscope              
+                
+    def avancer(self):
+        # Lecture de l'angle actuel du gyroscope
+        angle_actuel = self.gyroscope.angle
+        # Calcul de la correction à appliquer aux moteurs
+        correction = angle_actuel
+        # Utilisation de la correction pour ajuster les moteurs
+        self.moteurs.on(steering=correction, speed=70)
+    def reculer(self):
+        # Lecture de l'angle actuel du gyroscope
+        angle_actuel = self.gyroscope.angle
+        # Calcul de la correction à appliquer aux moteurs
+        correction = angle_actuel
+        # Utilisation de la correction pour ajuster les moteurs
+        self.moteurs.on(steering=correction, speed=-70)        
+    def stop(self) :
+        self.moteurs.off()        
 
 
 def main():
     try :
-        robot = Robot(OUTPUT_B, OUTPUT_A, None, INPUT_1, INPUT_2, None, None)
-        robot.avancer(1000)
-        x = 5/0
+        robot = Robot(OUTPUT_B, OUTPUT_A, None, INPUT_1, INPUT_2,INPUT_3, None, None)
+        robot.preparationRobot()
+        robot.avancerLigneDepart()
+        robot.avancerLigneArrivee()
+        robot.reculerLigneDepart()
+
+        
     except RobotException as r :
         print("Il y a eu un probleme avec le Robot. Arret de la simulation.")
         print(str(r))
